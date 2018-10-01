0ba0f51a23bac639e512417d3094ea71fa36d367
Phichat K <phichat_dk@hotmail.com>
2018-10-01 13:44:15 +0700

    commit

---
+++ src/app/core/http.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs/Observable';
+import {
+    Http,
+    RequestOptions,
+    RequestOptionsArgs,
+    Response,
+    Request,
+    Headers,
+    XHRBackend
+} from '@angular/http';
+
+import { AngularReduxRequestOptions } from './angular-redux-request.options';
+
+import { LoaderService } from './loader/loader.service';
+import { apiConfig } from '../app.config';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError, tap, finalize } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HttpService extends Http {
+    apiUrl = apiConfig.apiUrl;
+
+    constructor(
+        backend: XHRBackend,
+        defaultOptions: AngularReduxRequestOptions,
+        private loaderService: LoaderService
+    ) {
+        super(backend, defaultOptions);
+    }
+
+    get(url: string, options?: RequestOptionsArgs): Observable<any> {
+
+        this.showLoader();
+
+        return super.get(this.getFullUrl(url), this.requestOptions(options))
+            .pipe(
+                catchError(this.onCatch),
+                tap((res: Response) => {
+                    this.onSuccess(res);
+                }, (error: any) => {
+                    this.onError(error);
+                }),
+                finalize(() => this.onEnd())
+            );
+    }
+
+    private requestOptions(options?: RequestOptionsArgs): RequestOptionsArgs {
+
+        if (options == null) {
+            options = new AngularReduxRequestOptions();
+        }
+
+        if (options.headers == null) {
+            options.headers = new Headers();
+        }
+
+        return options;
+    }
+
+    private getFullUrl(url: string): string {
+        return this.apiUrl + url;
+    }
+
+    private onCatch(error: any, caught: Observable<any>): Observable<any> {
+        return Observable.throw(error);
+    }
+
+    private onSuccess(res: Response): void {
+        console.log('Request successful');
+    }
+
+    private onError(res: Response): void {
+        console.log('Error, status code: ' + res.status);
+    }
+
+    private onEnd(): void {
+        this.hideLoader();
+    }
+
+    private showLoader(): void {
+        this.loaderService.show();
+    }
+
+    private hideLoader(): void {
+        this.loaderService.hide();
+    }
+}
